0c618d1d1dd01ed7da631774e4fdb4fbcad045ae
chenyibo <chenyibo@niuguwang.com>
2024-03-04 11:35:19 +0800

    丰富网站

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
-function sayHello(){
-    alert('hello');
+let head = document.querySelector("h1");
+head.textContent = "前端练手记录";
+
+let image = document.querySelector("img");
+image.addEventListener("click", (s, e) => {
+  let imag_src = image.getAttribute("src");
+  if (imag_src === "images/header.jfif") {
+    image.src = "images/firefox-icon.png";
+  } else {
+    image.src = "images/header.jfif";
+  }
+});
+
+function sayHello() {
+  alert("hello");
 }
